13fc4af4bd8efba7b85d67ef0038ba2bfc8cd62b
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2017-12-26 19:53:18 +0900

    file upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import {Appbar, Container} from 'muicss/react';
 import Tasks from './components/Tasks';
+
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      tasks: []
+    }
+  }
+  componentWillMount(){
+    this.getTasks();
+  }
+  getTasks(){
+    axios.request({
+      method: 'get',
+      url: 'https://api.mlab.com/api/1/databases/reacttask/collections/tasks?apiKey=jTroFZtBVa3L0rbd70SoL9aovA5Om3j3'
+    }).then((response) => {
+      this.setState({tasks: response.data}, () => {
+        console.log(this.state)
+      })
+    }).catch((error) => {
+      console.log(error)
+    });
+  }
+
   render() {
     return (
       <div className="App">
